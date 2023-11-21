907e9982982db9178e4ca5b7aeb9693160802ca5
MananJoshi07 <146650284+MananJoshi07@users.noreply.github.com>
2023-11-21 23:32:37 +0530

    Update and rename demux.v to demux_1to4_case.v

--- Day4/demux_1to4_case.v
+++ Day4/demux_1to4_case.v
@@ -18,17 +18,17 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module demux(
+module demux_1to4_case(
              input [1:0] sel,
              input  i,
-             output reg y0,y1,y2,y3);
+  output reg [3:0] y);
     
   always @(*) begin
     case(sel)
-      2'h0: {y0,y1,y2,y3} = {i,3'b0};
-      2'h1: {y0,y1,y2,y3} = {1'b0,i,2'b0};
-      2'h2: {y0,y1,y2,y3} = {2'b0,i,1'b0};
-      2'h3: {y0,y1,y2,y3} = {3'b0,i};
+      2'h0: y[0] = i;
+      2'h1: y[1] = i;
+      2'h2: y[2] = i;
+      2'h3: y[3] = i;
       default: $display("Invalid sel input");
     endcase
   end
